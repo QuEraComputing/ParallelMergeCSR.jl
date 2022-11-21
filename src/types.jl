aeaf9eef6a35709a1572160a067f69bc8b92bdde
Phillip Weinberg <weinbe58@gmail.com>
2022-11-21 10:25:48 -0500

    moving from test to src

---
+++ src/types.jl
@@ -0,0 +1,9 @@
+abstract type AbstractSparseMatrixCSR{Tv,Ti} <: AbstractSparseMatrix{Tv,Ti} end
+
+struct SparseMatrixCSR{Tv,Ti} <: AbstractSparseMatrixCSR{Tv,Ti} 
+    m::Int                  # Number of rows
+    n::Int                  # Number of columns
+    rowptr::Vector{Ti}      # Row i is in rowptr[i]:(rowptr[i+1]-1)
+    colval::Vector{Ti}      # Col indices of stored values
+    nzval::Vector{Tv}       # Stored values, typically nonzeros
+end
