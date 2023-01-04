b033e3dcbc9f57cda21b6d82c4e0e18e407097e5
John Long <johnzl@protonmail.ch>
2023-01-04 12:34:39 -0500

    added complex-valued unit tests for merge_csr_mv, removed incompatible tests

--- test/merge_csr_mv!.jl
+++ test/merge_csr_mv!.jl
@@ -7,7 +7,7 @@ using SparseArrays
 ##       interfere
 @testset "Extreme Cases" begin
 
-    @testset "Singleton" begin
+    @testset "Singleton (Real)" begin
         A = sparse(reshape([1], 1, 1))
 
         x = rand(1)
@@ -19,43 +19,63 @@ using SparseArrays
         @test Matrix(A) * x * 0.3 == y
     end
 
-    @testset "Single row" begin
+    @testset "Singleton (Complex)" begin
+        A = sparse(reshape([1.0+2.5im], 1, 1))
+
+        x = rand(1)
+
+        y = zeros(eltype(A), size(A, 1))
+
+        merge_csr_mv!(10.1, A, x, y, adjoint)
+
+        @test adjoint(Matrix(A)) * x * 10.1 == y
+    end
+
+    @testset "Single row (Real)" begin
 
         # 10 x 1 converted to 1 x 10
-        A = SparseArrays.sprand(10, 1, 0.3)
+        A = 10.0 * SparseArrays.sprand(10, 1, 0.3)
 
         # x needs to be 10 x 1 
         x = rand(size(A, 1))
 
-        y = zeros(eltype(x), 1)
+        y = zeros(eltype(A), 1)
 
         merge_csr_mv!(1.1, A, x, y, transpose)
 
         @test (transpose(Matrix(A)) * x) * 1.1 ≈ y
     end
 
-    @testset "Single column" begin
+    @testset "Single row (Complex)" begin
 
-        # 1 x 10 is now 10 x 1
-        A = SparseArrays.sprand(1, 10, 0.3)
-        
-        # needs to be 1 x 1
-        x = rand(1)
+        # 10 x 1 is treated as 1 x 10 inside merge_csr_mv!
+        A = SparseArrays.sprand(Complex{Float64}, 10, 1, 0.3)
 
-        # 10 x 1
-        y = zeros(size(A,2))
-        
-        merge_csr_mv!(0.7, A, x, y, adjoint)
+        x = rand(eltype(A), size(A, 1))
 
-        y_exact = 0.7 * adjoint(Matrix(A)) * x
+        y = zeros(eltype(A), 1)
 
-        @test y_exact ≈ y
+        merge_csr_mv!(1.1, A, x, y, transpose)
+
+        @test (transpose(Matrix(A)) * x) * 1.1 ≈ y
     end
+
+
+    #= 
+        Single Column test wouldn't work because 
+        merge_csr_mv! is designed to handle Ax = y where
+        A is m x n, x is n x 1, and y must be m x 1.
+
+        Given a single column A that is 1 x z, merge_csr_mv! treats it as 
+        z x 1, which means y is constrained to 1 x 1 and the correct
+        output would be z x 1, which merge_csr_mv! is not designed for. 
+
+        On the other hand, the overridden SparseArrays.mul! should work
+    =#
 end
 
 # test fails, y seems to have lots of zero-entries
-@testset "Square" begin
-    # 10 x 10 with 30% chance of entry being made
+@testset "Square (Real)" begin
     A = SparseArrays.sprand(10,10,0.3)
 
     # 10 x 1
@@ -70,8 +90,20 @@ end
 
 end
 
-@testset "4x6" begin
-    # create matrix
+@testset "Square (Complex)" begin
+    A = SparseArrays.sprand(Complex{Float64}, 10, 10, 0.3)
+
+    x = 10 * rand(Complex{Float64}, 10)
+
+    y = zeros(eltype(A), size(x))
+
+    merge_csr_mv!(1.1, A, x, y, adjoint)
+
+    @test (adjoint(Matrix(A)) * x) * 1.1 ≈ y
+end
+
+@testset "4x6 (Real)" begin
+    # Taken from: https://en.wikipedia.org/wiki/Sparse_matrix
     m = [10 20 0 0 0 0;
          0 30 0 40 0 0;
          0 0 50 60 70 0;
@@ -81,7 +113,6 @@ end
     A = sparse(m)
 
     # create vector
-    # x = [5,2,3,1,8,2]
     x = [5,2,3,1]
 
     # create empty solution
@@ -94,7 +125,30 @@ end
     @test Matrix(adjoint(m)) * x * 2.0 == y
 end
 
-@testset "100x100" begin
+@testset "4 x 6 (Complex)" begin
+    # Taken from: https://en.wikipedia.org/wiki/Sparse_matrix
+    m = [10 20 0 0 0 0;
+    0 30 0 40 0 0;
+    0 0 50 60 70 0;
+    0 0 0 0 0 80]
+
+    # get into CSC form
+    A = sparse(m)
+
+    # create vector
+    x = 22.1 * rand(Complex{Float64}, 4)
+
+    # create empty solution
+    y = zeros(eltype(x), size(A, 2))
+
+    # multiply
+    merge_csr_mv!(2.0, A, x, y, adjoint)
+
+    @test Matrix(adjoint(m)) * x * 2.0 == y
+
+end
+
+@testset "100x100 (Real)" begin
     # create matrix
     A = SparseArrays.sprand(100, 100, 0.3)
 
@@ -109,29 +163,63 @@ end
     @test transpose(Matrix(A)) * x * 3 ≈ y
 end
 
-@testset "Matrix x Matrix" begin
+@testset "100x100 (Complex)" begin
+    # create matrix
+    A = SparseArrays.sprand(Complex{Float64}, 100, 100, 0.3)
 
-    ## Calculate AXα
+    # create vector
+    x = rand(Complex{Float64}, 100)
+
+    # create empty solution
+    y = zeros(eltype(x), size(A, 1))
+
+    merge_csr_mv!(3.0, A, x, y, transpose)
+
+    @test transpose(Matrix(A)) * x * 3 ≈ y
+end
+
+#=
+    NOTE: While merge_csr_mv! can be used this way, the overriden SparseArrays.mul!
+    should be the preferred method to do Matrix-Matrix multiplication.
+=#
+@testset "Matrix-Matrix (Real)" begin
+
     # Create Matrices
-    # 3 x 2
+    # 3 x 2 (treated as 2 x 3 in merge_csr_mv!)
     A = sprand(3, 2, 0.4)
     # 3 x 4
     X = rand(3, 4)
 
-    # set alpha to 1.0 for now
-    α = 1.0 
+    α = 9.2
 
-    # Create place to store solution, 
-    # should be 2 x 4
     Y = zeros(2, 4)
 
-    # iterate
-    # some kind of indexing issue going on here
     for (idx, col) in enumerate(eachcol(X))
         Y_view = @view Y[:, idx]
         merge_csr_mv!(α, A, col, Y_view, transpose)
     end
 
-    @test transpose(Matrix(A)) * X * 1.0 ≈ Y 
+    @test transpose(Matrix(A)) * X * 9.2 ≈ Y 
+
+end
+
+@testset "Matrix-Matrix (Real)" begin
+
+    # Create Matrices
+    # 3 x 2 (treated as 2 x 3 in merge_csr_mv!)
+    A = sprand(Complex{Float64}, 3, 2, 0.4)
+    # 3 x 4
+    X = rand(3, 4)
+
+    α = 9.2
+
+    Y = zeros(eltype(A), 2, 4)
+
+    for (idx, col) in enumerate(eachcol(X))
+        Y_view = @view Y[:, idx]
+        merge_csr_mv!(α, A, col, Y_view, adjoint)
+    end
+
+    @test adjoint(Matrix(A)) * X * 9.2 ≈ Y 
 
 end
